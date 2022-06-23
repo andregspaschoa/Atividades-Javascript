f45d8dae3debebfc46eee6004395b908cc58f83d
andregspaschoa <paschoa.andre@outlook.com>
2022-06-23 18:01:23 -0300

    Create aula2.js

---
+++ Curso JavaScript/aula2.js
@@ -0,0 +1,11 @@
+// Tipos primitivos // 
+
+let pessoa2 
+{
+let nome = 'Gabriel'; //string literal
+let idade = 26.5; //number literal
+let estaAprovado = true; //boolean
+let sobrenome ; //Undefined
+let corSelecionado = null //Redefinir um valor
+}
+
