285b4eeac302425f05140c34fffb375677ecff67
andregspaschoa <paschoa.andre@outlook.com>
2022-06-23 20:18:44 -0300

    Create aula6.js

---
+++ Curso JavaScript/aula6.js
@@ -0,0 +1,38 @@
+/* Operadores */
+// Aritimeticos, atribuição, comparação, logicos e Bitwise //
+/* Aritiméticos */ /* +, -, *, /, */
+
+let salario = 100;
+
+console.log(salario + salario);
+console.log(salario - salario);
+console.log(salario * salario);
+console.log(salario / salario);
+console.log(5 ** 5);
+// ++ -- 
+
+let idade2 = 18;
+console.log(-- idade2);
+console.log(idade2);
+
+/* operadores de atribuição */ 
+
+let valorTecladoGamer = 100;
+
+valorTecladoGamer += valorTecladoGamer;
+
+console.log(valorTecladoGamer);
+
+/* Operadores de igualdade */
+
+// igualdade estrita
+
+console.log (1 === 1);
+console.log('1' === 1);
+
+// Igualdade solta 
+
+console.log ( 1 == 1);
+console.log ('1' == 1);
+
+/* essa forma não é a mais correta de se comparar */ 
