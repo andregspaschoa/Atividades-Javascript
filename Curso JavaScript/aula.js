2c79fcf28e48c13b575c72138db876dab93df0a8
andregspaschoa <paschoa.andre@outlook.com>
2022-06-23 18:01:26 -0300

    Update aula.js

--- Curso JavaScript/aula.js
+++ Curso JavaScript/aula.js
@@ -1,33 +1,31 @@
 /* Primeira aula de javascript */
 /* Declarando variaveis */ 
 
-let idade = 26; 
+ let idade = 26; 
 /* Foi utilizado let como variavel dado o valor de idade e o valor da variavel após o sinal de igual */
-console.log(idade)
+ console.log(idade)
 
-let altura = 180;
+ let altura = 180;
 
-/* não iniciar uma variavel com numeros ou letras maiusculas */
+ /*não iniciar uma variavel com numeros ou letras maiusculas */
 
-let nome = 'Andre'
+ let nome = 'Andre';
 
-console.log(nome)
+ console.log(nome)
 
-console.log(altura)
+ console.log(altura)
 
 
-const valorIngressoAdulto = 30;
+ const valorIngressoAdulto = 30;
 
-console.log(valorIngressoAdulto)
+ console.log(valorIngressoAdulto)
 
 /* Conforme as boas praticas é preferivel utilizar a variavel const quando o valor for fixo */ 
 
-/* Tipos primitivos */ 
 
-let nome = 'Andre' //string literal
-let idade = 26; //number literal
-let estaAprovado = true; //boolean
-let sobrenome ; //Undefined
-let corSelecionado = null //Redefinir um valor
- 
+
+
+
+
+
 
