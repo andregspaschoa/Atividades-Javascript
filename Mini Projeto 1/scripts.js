bd3a39f0284b595b4f860ce5d9cc36360072a9d9
andregspaschoa <paschoa.andre@outlook.com>
2022-06-25 17:03:41 -0300

    Create scripts.js
    Mini projeto 1
    
    Trocando valores de variáveis.

---
+++ Mini Projeto 1/scripts.js
@@ -0,0 +1,22 @@
+console.log('Mini Projeto 1')
+console.log('')
+console.log('Trocando valores de variáveis')
+console.log('')
+let a = 'Vermelho';
+let b = 'Azul';
+
+console.log('Valor da variável a =', a);
+console.log('Valor da variável b =', b);
+console.log('')
+console.log('Invertendo os valores das variáveis:')
+console.log('')
+
+
+let c = a;  // o valor da variavel a foi armazenado dentro da 'c' 
+
+a = b;
+
+b = c;
+
+console.log('Variável a =',a);
+console.log('Variável b =',b);
