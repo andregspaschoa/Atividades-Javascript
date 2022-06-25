6f03e577fb36446e0495fd141f92716b8aff68b0
andregspaschoa <paschoa.andre@outlook.com>
2022-06-25 17:25:58 -0300

    Update aula.js

--- Curso JavaScript/aula.js
+++ Curso JavaScript/aula.js
@@ -1,6 +1,6 @@
 /* Primeira aula de javascript */
-/* Declarando variaveis */ 
-
+console.log('Aula 1')
+console.log(' ')
  let idade = 26; 
 /* Foi utilizado let como variavel dado o valor de idade e o valor da variavel após o sinal de igual */
  console.log(idade)
@@ -21,6 +21,7 @@
  console.log(valorIngressoAdulto)
 
 /* Conforme as boas praticas é preferivel utilizar a variavel const quando o valor for fixo */ 
+console.log(' ')
 
 
 
