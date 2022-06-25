fba4c453092731f5d112b6bb2a88fe106dc743fc
andregspaschoa <paschoa.andre@outlook.com>
2022-06-25 17:26:05 -0300

    Update aula4.js

--- Curso JavaScript/aula4.js
+++ Curso JavaScript/aula4.js
@@ -1,6 +1,7 @@
  /* Arrays */ 
 // Conjunto de dados que pode ser acessado por um indice
-
+console.log('Aula 4')
+console.log(' ')
 let familia = [ 26,45,50,17];
 console.log(familia.length);
 console.log(familia[0]);
