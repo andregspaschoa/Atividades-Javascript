ab2bbd68d0029fda5021a50b93a740aad080ed5f
andregspaschoa <paschoa.andre@outlook.com>
2022-06-26 13:58:10 -0300

    Delete aula4.js

--- Curso JavaScript/aula4.js
+++
@@ -1,8 +0,0 @@
- /* Arrays */ 
-// Conjunto de dados que pode ser acessado por um indice
-console.log('Aula 4')
-console.log(' ')
-let familia = [ 26,45,50,17];
-console.log(familia.length);
-console.log(familia[0]);
-
