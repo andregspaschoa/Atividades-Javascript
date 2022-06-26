6330dce10de07372cf00813ef6fffc8e924b9c1d
andregspaschoa <paschoa.andre@outlook.com>
2022-06-26 13:57:15 -0300

    Create aula.js

---
+++ Curso JavaScript/1-Variáveis e Constantes/aula.js
@@ -0,0 +1,32 @@
+/* Primeira aula de javascript */
+console.log('Aula 1')
+console.log(' ')
+ let idade = 26; 
+/* Foi utilizado let como variavel dado o valor de idade e o valor da variavel após o sinal de igual */
+ console.log(idade)
+
+ let altura = 180;
+
+ /*não iniciar uma variavel com numeros ou letras maiusculas */
+
+ let nome = 'Andre';
+
+ console.log(nome)
+
+ console.log(altura)
+
+
+ const valorIngressoAdulto = 30;
+
+ console.log(valorIngressoAdulto)
+
+/* Conforme as boas praticas é preferivel utilizar a variavel const quando o valor for fixo */ 
+console.log(' ')
+
+
+
+
+
+
+
+
