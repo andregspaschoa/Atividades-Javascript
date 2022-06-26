a251af6cfcb93de03289b55a0cd086d1efa07e1c
andregspaschoa <paschoa.andre@outlook.com>
2022-06-26 16:43:15 -0300

    Create script.js

---
+++ Mini Projeto 2/script.js
@@ -0,0 +1,13 @@
+let valorMaior = max(499,49);
+console.log(valorMaior);
+
+function max(numero1,numero2) {
+//   if(numero1 > numero2)
+//       return numero1;     Essa condição é valida porém veja abaixo:
+//    else return numero2;
+
+    return numero1 > numero2 ? numero1 : numero2 ;
+/* Esse exemplo torna o codigo mais limpo */
+
+}
+
