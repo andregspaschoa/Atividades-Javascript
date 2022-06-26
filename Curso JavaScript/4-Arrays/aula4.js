b6ca213dd4da871e812e6124b97bd355c480cc5d
andregspaschoa <paschoa.andre@outlook.com>
2022-06-26 13:57:29 -0300

    Create aula4.js

---
+++ Curso JavaScript/4-Arrays/aula4.js
@@ -0,0 +1,8 @@
+ /* Arrays */ 
+// Conjunto de dados que pode ser acessado por um indice
+console.log('Aula 4')
+console.log(' ')
+let familia = [ 26,45,50,17];
+console.log(familia.length);
+console.log(familia[0]);
+
