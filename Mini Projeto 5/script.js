6b4d485cfef77f1ebfb56767a3b2074bd668985f
Andre G. S. Paschoa <108034536+andregspaschoa@users.noreply.github.com>
2022-06-28 14:26:18 -0300

    Create script.js

---
+++ Mini Projeto 5/script.js
@@ -0,0 +1,12 @@
+// Receber ouma quantidade de valores para avaliar
+// função exibe se cada valor é par ou ímpar
+
+exibirTipo(10);
+function exibirTipo(limite) {
+ for (let i = 0; i <= limite; i++)   {
+    if ( i % 2 === 0)
+          console.log(i,'PAR');
+    else
+       console.log(i,'IMPAR');
+ }
+}
