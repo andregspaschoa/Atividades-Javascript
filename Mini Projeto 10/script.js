716d21a9742c02a839f05476e1c68a6f61907823
Andre G. S. Paschoa <108034536+andregspaschoa@users.noreply.github.com>
2022-06-30 12:30:29 -0300

    Update script.js

--- Mini Projeto 10/script.js
+++ Mini Projeto 10/script.js
@@ -10,9 +10,9 @@ function exibirNumerosPrimos(limite) {
 
 function numeroPrimo(numero) {
     for (let divisor = 2; divisor < numero; divisor++){
-        if(numero % divisor === 0) {
+        if(numero % divisor === 0) {        
             return false;
-        }
-    }
-    return true;
+        }          // numeros primos são divisíveis somente por 1 ou por eles mesmos
+    }              // sendo assim se o resultado da divisão for 0 retornará falso caso contrario sera verdadeiro  
+    return true;   // e somente sendo numero primo será retornado no console
 }
