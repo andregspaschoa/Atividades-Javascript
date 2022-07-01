93adaf6d7af0cfdd84ef757509be59b4679bea7d
Andre G. S. Paschoa <108034536+andregspaschoa@users.noreply.github.com>
2022-07-01 16:42:20 -0300

    Update script.js

---
+++ Curso JavaScript/15 - String/script.js
@@ -0,0 +1,9 @@
+// String
+//Tipo primitivo
+
+const mensagem = 'minha primeira mensagem';
+
+//Tipo objeto
+const outraMensagem = new String('bom dia')
+
+//utilize o o typeof no console para verificar a diferença
