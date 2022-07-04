e01492f28a5d907dd005ce04fe86b7a4afbcef17
andregspaschoa <paschoa.andre@outlook.com>
2022-07-03 21:56:19 -0300

    Create script.js

---
+++ Curso JavaScript/16-Template literal/script.js
@@ -0,0 +1,23 @@
+const mensagem = 'Oi isso é a minha\n \'primeira\' mensagem'; //forma antiga
+
+//Objetc {}
+//Boolean true e false
+//string '',""
+//template ``
+
+const outra = `Oi isso é a minha
+'primeira' mensagem`;
+
+console.log(mensagem);
+console.log(outra);
+
+const nome = 'Rafaela';
+
+const email = 
+`Olá, ${nome},   
+
+Obrigado por acessar o nosso site  
+Att`; 
+//podemos declarar aqui qualquer metodo ou função de js também .
+
+console.log(email);
