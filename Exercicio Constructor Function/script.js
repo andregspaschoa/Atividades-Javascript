43bc8983d1b08506bc60fe737c1373910996455e
andregspaschoa <paschoa.andre@outlook.com>
2022-07-03 21:57:06 -0300

    Create script.js

---
+++ Exercicio Constructor Function/script.js
@@ -0,0 +1,11 @@
+function Postagem(titulo,mensagem,autor){
+    this.titulo = titulo,
+    this.mensagem = mensagem,
+    this.autor = autor,
+    this.visualizacoes= 0,
+    this.comentarios = [],
+    this.estaAoVivo = false 
+}
+
+let postagem = new Postagem ('a','b','c');
+console.log(postagem);
